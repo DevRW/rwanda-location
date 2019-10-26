7f0b7ed70c67531c3e425e7925df8c3d45369795
Theo Okafor <theo.okafor@yahoo.com>
2019-10-26 23:52:12 +0200

    initialise the express server in index.js file

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-// import express from 'express';
-// import morgan from 'morgan';
-// import dotenv from 'dotenv';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-// dotenv.config();
+// Set up express app
+const app = express();
 
-// // Set up express app
-// const app = express();
+// Log requests
+app.use(morgan('dev'));
 
-// // Log requests
-// app.use(morgan('dev'));
+app.use(cors());
 
-// app.use(cors());
+// Parse the body of incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// // Parse the body of incoming requests
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+const port = process.env.PORT || 8080;
 
-// const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on port ${port}`);
+});
 
-// app.listen(port, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`Server listening on port ${port}`);
-// });
-
-// export default app;
+export default app;
